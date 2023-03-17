2de7aa98b5172e64975cdd9287d857b8df87bd07
gmnavidrouz <navidrouz@gmail.com>
2023-03-17 09:44:56 -0700

    Update 1

---
+++ Swift UI Camera/Swift_UI_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_CameraApp.swift
+//  Swift UI Camera
+//
+//  Created by Navid Rouzroch on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
